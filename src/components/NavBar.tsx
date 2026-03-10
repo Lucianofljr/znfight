3c29e50bc94a271b824684096f11db2aa0a96b9d
Lucianofljr <lucianoflirajr@gmail.com>
2026-03-10 09:42:12 -0300

    feat: Criacao da NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,12 @@
 import styles from './NavBar.module.css'
 
+import { Menu } from './Menu'
+
 export function NavBar() {
     return (
         <>
             <div className={styles.navbar}>
+                <Menu />
             </div>
         </>
     )
